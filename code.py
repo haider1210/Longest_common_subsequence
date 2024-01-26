da094a2b9b064e0147434962bd7adb6009ea8ca1
Md Haider Ali <121740404+haider1210@users.noreply.github.com>
2024-01-26 10:21:29 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,16 +1,17 @@
-#Brute force 
+#Bottom-Up aaproach -Tabulation form
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        def lcs_recursive(i, j):
-            if i == len(text1) or j == len(text2):
-                return 0
+        n = len(text1)
+        m = len(text2)
 
-            if text1[i] == text2[j]:
-                return 1 + lcs_recursive(i + 1, j + 1)
-            else:
-                return max(lcs_recursive(i + 1, j), lcs_recursive(i, j + 1))
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
 
-        return lcs_recursive(0, 0)
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
+        return dp[n][m]
 
-##
